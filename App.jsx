2d59f367f989f191b85c6de4a7c83a2fced9f43e
Francisco Peralta <franp@mantik.tech>
2020-09-04 21:10:34 -0300

    add sccafolding

---
+++ App.jsx
@@ -0,0 +1,15 @@
+import 'react-native-gesture-handler';
+import React from 'react';
+import {
+  Text
+} from 'react-native';
+
+const App = () => {
+  return (
+    <>
+      <Text>Buyed App</Text>
+    </>
+  );
+};
+
+export default App;
---
+++ src/utils/colors.js
@@ -0,0 +1,2 @@
+export default black = "#000";
+export default white = "#fff";
